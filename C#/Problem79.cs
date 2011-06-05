89068e8405c9c3f04e5118abc5d20a8b8a323fb5
Chethan Prakash <yourschethu@gmail.com>
2011-06-05 23:33:47 +0530

    Adding solution to problem 79

--- C#/Problem79.cs
+++ C#/Problem79.cs
@@ -2,21 +2,39 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
 using EulerProblem.Utils;
 
 namespace EulerProblem
 {
     public static class Problem79
     {
-        
         public static string MinimumPossiblePassword()
         {
             var inputPasswords = PruneInputData();
-            return "";
+            var finalPassword = new Dictionary<int, int>(10);
+            foreach (var digits in inputPasswords.Select(inputPassword => MathUtils.GetDigits(inputPassword)))
+            {
+                for (var i = 0; i < digits.Count; i++)
+                {
+                    if (!finalPassword.ContainsKey(digits[i])) finalPassword[digits[i]] = i;
+                    else
+                    {
+                        var tempOrder = i + (i>0?finalPassword[digits[i-1]]:0);
+                        if (tempOrder > finalPassword[digits[i]]) finalPassword[digits[i]] = tempOrder;
+                    }
+                }
+            }
+            var builder = new StringBuilder();
+            foreach (var keyValuePair in finalPassword.OrderByDescending(pair => pair.Value))
+            {
+                builder.Append(keyValuePair.Key);
+            }
+            return builder.ToString();
         }
 
 
-        private static List<int> PruneInputData()
+        private static IEnumerable<int> PruneInputData()
         {
             var threeCharectersOfPasswords = new HashSet<int>();
             using (var fileStream = File.OpenText("Problem79.txt"))
@@ -26,14 +44,7 @@ namespace EulerProblem
                     threeCharectersOfPasswords.Add(Convert.ToInt32(fileStream.ReadLine()));
                 }
             }
-            using (var fileStream = File.CreateText("C:\\a.txt"))
-            {
-                foreach (var threeCharectersOfPassword in threeCharectersOfPasswords.OrderBy(i => i))
-                {
-                    fileStream.WriteLine(threeCharectersOfPassword);
-                }
-            }
-            return threeCharectersOfPasswords.OrderBy(i => i).ToList();
+            return threeCharectersOfPasswords;
         }
     }
 }
