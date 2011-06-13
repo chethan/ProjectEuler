f3d4fe7fb754e486f65780932f9d51f50e0f453b
Chethan Prakash <yourschethu@gmail.com>
2011-06-13 18:20:32 +0530

    Problem 277 using haskell, removed redundant code,still looking for a better solution

--- Haskell/ModifiedCollatzSequence(277).hs
+++ Haskell/ModifiedCollatzSequence(277).hs
@@ -1,10 +1,3 @@
-check_step num 'D' = (rem num 3) == 0
-check_step num 'd' = (rem num 3) == 2
-check_step num 'U' = (rem num 3) == 1
-
-apply_step num 'D' = div num 3
-apply_step num 'd' = div ((num * 2)-1) 3
-apply_step num 'U' = div ((num * 4)+2) 3
 
 reverse_step num 'D' = num *3 
 reverse_step num 'd' = div ((num *3)+1) 2  
@@ -13,13 +6,6 @@ reverse_step num 'U' = div ((num *3)-2) 4
 check_reverse_step num 'D' = True 
 check_reverse_step num 'd' = (rem ((num*3)+1) 2)==0 
 check_reverse_step num 'U' = (rem ((num*3)-2) 4)==0 
-
-check_collatz [] _ = True
-check_collatz (x:xs) num | (check_step num x) = check_collatz xs (apply_step num x)
-		         | otherwise = False
-
-first_collatz = head $ filter (check_collatz "UDDDUdddDDUDDddDdDddDDUDDdUUDd") [(10^15)..] 
-
 
 generate_collatz_num [] num = num
 generate_collatz_num (x:xs) num | (check_reverse_step num x) = generate_collatz_num xs (reverse_step num x)
